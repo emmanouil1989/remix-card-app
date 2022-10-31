ed7dd6a566d2911e5b7145a95b8dddea83482e86
Emmanouil Emmanouil <emmanouil.e25@gmail.com>
2022-10-31 08:33:48 +0000

    add seed file and install ts-node

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+process.env.RUNNING_PRETTIER = 'true'
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: false,
+    embeddedLanguageFormatting: 'auto',
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'css',
+    insertPragma: false,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: 'always',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: false,
+    trailingComma: 'all',
+}
