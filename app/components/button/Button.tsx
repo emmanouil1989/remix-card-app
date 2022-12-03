3e84164ed03dab83c4f50ac9cd3e07342a175e5f
Emmanouil Emmanouil <emmanouil.e25@gmail.com>
2022-12-03 15:14:37 +0000

    make styles change to login form

---
+++ app/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import {ComponentPropsWithRef, MouseEvent, PropsWithChildren} from "react"
+
+export type ButtonProps = ComponentPropsWithRef<"button">
+export default function Button({onClick, children}: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+    >
+      {children}
+    </button>
+  )
+}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,30 +1,34 @@
 import {useSearchParams} from "@remix-run/react"
+import Button from "~/components/button/Button"
 
 export default function Login() {
   const [params] = useSearchParams()
 
   return (
     <section
-      className={"w-full h-full flex justify-center items-center flex-col"}
+      className={
+        "w-full h-full flex justify-center items-center flex-col bg-gray-100"
+      }
     >
       <h1>Login</h1>
-      <form method={"post"} className={"border bg-gray-400 w-1/6 h-1/6"}>
+      <form method={"post"} className={" min-h-max p-8 w-4/12 "}>
         <input
           type="hidden"
           name="redirectTo"
           value={params.get("redirectTo") ?? undefined}
         />
-        <div>
-          <label htmlFor="email-input">Email</label>
+        <div className={"flex flex-col justify-between py-4"}>
+          <label htmlFor="email-input">Email:</label>
           <input type={"email"} name={"email"} id={"email-input"} />
         </div>
-        <div>
-          <label htmlFor="password-input">Password</label>
+        <div className={"flex flex-col justify-between py-4"}>
+          <label htmlFor="password-input">Password:</label>
           <input type={"password"} name={"password"} id={"password-input"} />
         </div>
-
-        <button type={"submit"}>Sign up</button>
-        <button type={"submit"}>Login</button>
+        <div className={"flex row justify-between items-center w-full"}>
+          <Button type={"submit"}>Sign up</Button>
+          <Button type={"submit"}>Login</Button>
+        </div>
       </form>
     </section>
   )
