b0fc7177408d76420dff141f582c600db0ac3469
Emmanouil Emmanouil <emmanouil.e25@gmail.com>
2022-12-12 09:11:18 +0200

    changes to schema and seed file

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -5,7 +5,7 @@ export default function Button({onClick, children}: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+      className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
     >
       {children}
     </button>
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,34 @@
 import {PrismaClient} from "@prisma/client"
+import {faker} from "@faker-js/faker"
+import bycript from "bcrypt"
 
 const prisma = new PrismaClient()
 
+//import faker and write a user factory function which generate random data for the user
+
+function userFactory() {
+  const hashedPassword = bycript.hash("password1", 10)
+
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: hashedPassword,
+    mobilePhone: faker.phone.number(),
+    role: "USER",
+  }
+}
 async function main() {
-  // await prisma.user.create({
-  //   data: {
-  //     name: "Alice",
-  //     email: "",
-  //   },
-  // })
+  await prisma.user.create({
+    data: {
+      firstName: "manos",
+      lastName: "koukis",
+      email: "emmanouil.e@hotmail.com",
+      password: "$2a$04$2t9xcOy71K4QqkENc2NWS.c3sVrfrOKC8DSH5n39dDMbTb.HSLWEK",
+      mobilePhone: "6971234567",
+      role: "ADMIN",
+    },
+  })
 }
 
 main()
