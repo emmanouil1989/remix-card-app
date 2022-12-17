87e538eb1b55f99389bf7c4bad67da0600cc4220
Emmanouil Emmanouil <emmanouil.e25@gmail.com>
2022-12-17 19:24:40 +0200

    change schema and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,60 @@
-import {PrismaClient} from "@prisma/client"
-import {faker} from "@faker-js/faker"
-import bycript from "bcrypt"
+import { PrismaClient, Role, Prisma } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import bycript from "bcrypt";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-//import faker and write a user factory function which generate random data for the user
-
-function userFactory() {
-  const hashedPassword = bycript.hash("password1", 10)
+async function userFactory(
+  index: number,
+  storeId: string,
+): Promise<Prisma.UserCreateInput> {
+  const hashedPassword = await bycript.hash("password1", 10);
 
   return {
     firstName: faker.name.firstName(),
     lastName: faker.name.lastName(),
-    email: faker.internet.email(),
+    email: `pelatis${index}@gmail.com`,
     password: hashedPassword,
     mobilePhone: faker.phone.number(),
-    role: "USER",
-  }
-}
-async function main() {
-  await prisma.user.create({
-    data: {
-      firstName: "manos",
-      lastName: "koukis",
-      email: "emmanouil.e@hotmail.com",
-      password: "$2a$04$2t9xcOy71K4QqkENc2NWS.c3sVrfrOKC8DSH5n39dDMbTb.HSLWEK",
-      mobilePhone: "6971234567",
-      role: "ADMIN",
+    role: Role.USER,
+    bookings: {
+      create: bookingsFactory(storeId),
     },
-  })
+  };
+}
 
+function bookingsFactory(
+  storeId: string,
+): Array<Prisma.BookingCreateWithoutUserInput> {
+  return [
+    {
+      store: {
+        connect: {
+          id: storeId,
+        },
+      },
+      start: faker.date.future(),
+      end: faker.date.future(),
+    },
+  ];
+}
+
+function servicesFactory(): Array<Prisma.ServiceCreateWithoutStoreInput> {
+  return ["Male Haircut", "Beard Trim", "Shave", "Haircut & Hair Wash"].map(
+    function mapToService(serviceName) {
+      return {
+        name: serviceName,
+        price: Number(faker.commerce.price()),
+      };
+    },
+  );
+}
+
+async function main() {
+  await prisma.store.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.service.deleteMany();
+  await prisma.booking.deleteMany();
   await prisma.store.create({
     data: {
       name: "Barber Shop 1963",
@@ -39,8 +64,32 @@ async function main() {
       postalCode: "52100",
       phone: faker.phone.number(),
       email: faker.internet.email(),
+      services: {
+        create: servicesFactory(),
+      },
     },
-  })
+  });
+
+  const store = await prisma.store.findFirst();
+
+  if (store) {
+    for (let i = 0; i < 10; i++) {
+      await prisma.user.create({
+        data: await userFactory(i + 1, store.id),
+      });
+    }
+  }
+
+  await prisma.user.create({
+    data: {
+      firstName: "manos",
+      lastName: "koukis",
+      email: "emmanouil.e@hotmail.com",
+      password: "$2a$04$2t9xcOy71K4QqkENc2NWS.c3sVrfrOKC8DSH5n39dDMbTb.HSLWEK",
+      mobilePhone: "6971234567",
+      role: Role.ADMIN,
+    },
+  });
 }
 
-main()
+main();
